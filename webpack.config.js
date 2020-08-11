adfb281a2742c02a2cdf537b5b270b49778d0ec5
arcturus2 <39325530+arcturus2@users.noreply.github.com>
2020-08-12 08:32:14 +1000

    :art: First release of the new website * Restructured folders * Added webpack and npm * Added wording to all sections * Added a downloads page * Added a placeholder image for updates * Added a gif animation to the background * Webpack now compiles scss files * Fixed compile warnings to sass files * Autoformat code * dos2unix * Deleted unwanted images

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    entry: [
+        './src/js/util.js',
+        './src/js/main.js',
+        './src/sass/main.scss'
+    ],
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'public_html/assets/js')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '../css/app.css',
+                        }
+                    },
+                    {
+                        loader: 'extract-loader'
+                    },
+                    {
+                        loader: 'css-loader?-url'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            options: {},
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    }
+};
