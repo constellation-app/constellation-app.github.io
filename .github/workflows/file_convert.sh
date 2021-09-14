36e627ea4160df2fb8783d3bda21e724e85879bb
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-09-15 07:55:56 +0930

    add template18

--- .github/workflows/file_convert.sh
+++ .github/workflows/file_convert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "converting .md to .html using template"; find */src/ -path '*/docs/*' -name "*.md" | while read x; do pandoc --template="const/onlinePandocTemplate.txt" -f commonmark -t html $x > "$x".html; done; echo "done converting...."; echo "renaming all .md.html to .html"; find */src/ -name *.md.html | while read x; do  mv "$x" "${x%.md.html}".html; done; echo "done renaming...."; echo "Moving all html png jpg to /pages/docs/constydocs"; find */src/ -path '*/docs/*' -name "*html" \
-| xargs cp --parents -t /pages/docs/constydocs; find */src/ -path '*/docs/*' -name "*.png" | xargs cp --parents -t /pages/docs/constydocs; find */src/ -path '*/docs/*' -name "*.jpg" | xargs cp --parents -t /pages/docs/constydocs
+| xargs cp --parents -t ./pages/docs/constydocs; find */src/ -path '*/docs/*' -name "*.png" | xargs cp --parents -t ./pages/docs/constydocs; find */src/ -path '*/docs/*' -name "*.jpg" | xargs cp --parents -t ./pages/docs/constydocs
