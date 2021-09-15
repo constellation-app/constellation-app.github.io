3566b10611b1e37a7b3e76a5b8b499630998f39b
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-09-16 09:23:05 +0930

    xargs 2

--- .github/workflows/file_convert.sh
+++ .github/workflows/file_convert.sh
@@ -3,8 +3,8 @@
 echo "converting links from .md to .html"
 find ./constellation/* -path '*docs/*' -name "*.md" | xargs -I{} sed -i -e 's/.md/.html/g' {}
 echo "converting .md to .html using template"
-find ./constellation/* -path '*/docs/*' -name "*.md" | xargs -I{} pandoc --template="./constellation/onlinePandocTemplate.txt" -f commonmark -t html {} > {}.html; done
+find ./constellation/* -path '*/docs/*' -name "*.md" | xargs -I{} pandoc --template="./constellation/onlinePandocTemplate.txt" -f commonmark -t html {} > {}.html
 echo "done converting...."
 echo "renaming all .md.html to .html"
-find ./constellation/* -name *.md.html | while read x; do  mv "$x" "${x%.md.html}".html; done
+find ./constellation/* -name *.md.html | xargs -I{} mv {} {}.html
 echo "done renaming...."
