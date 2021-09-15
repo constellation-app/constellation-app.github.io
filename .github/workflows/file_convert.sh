e242989283627ece13cb6fa8dc559519c602e5cc
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-09-16 09:17:20 +0930

    refactor

--- .github/workflows/file_convert.sh
+++ .github/workflows/file_convert.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "converting links from .md to .html"; find ./constellation/* -path '*docs/*' -name "*.md" | xargs -I{} sed -i -e 's/.md/.html/g' {}; echo "converting .md to .html using template"; find ./constellation/* -path '*/docs/*' -name "*.md" | while read x; do pandoc --template="./constellation/onlinePandocTemplate.txt" -f commonmark -t html $x > "$x".html; done; echo "done converting...."; echo "renaming all .md.html to .html"; find ./constellation/* -name *.md.html | while read x; do  mv "$x" "${x%.md.html}".html; done; echo "done renaming....";
+echo "converting links from .md to .html"
+find ./constellation/* -path '*docs/*' -name "*.md" | xargs -I{} sed -i -e 's/.md/.html/g' {}
+echo "converting .md to .html using template"
+find ./constellation/* -path '*/docs/*' -name "*.md" | while read x; do pandoc --template="./constellation/onlinePandocTemplate.txt" -f commonmark -t html $x > "$x".html; done
+echo "done converting...."
+echo "renaming all .md.html to .html"
+find ./constellation/* -name *.md.html | while read x; do  mv "$x" "${x%.md.html}".html; done
+echo "done renaming...."
--- .github/workflows/file_copy.sh
+++ .github/workflows/file_copy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "Moving all html png jpg to pages/docs"; find ./constellation/* -path '*/docs/*' -name "*html" | xargs -I{} cp {} --parents -t ./pages/docs/help/; find ./constellation/* -path '*/docs/*' -name "*.png" | xargs -I{} cp {} --parents -t ./pages/docs/help/; find ./constellation/* -path '*/docs/*' -name "*.jpg" | xargs -I{} cp {} --parents -t ./pages/docs/help;
+echo "Moving all html png jpg to pages/docs/help..."
+find ./constellation/* -path '*/docs/*' -name "*html" | xargs -I{} cp {} --parents -t ./pages/docs/help/
+find ./constellation/* -path '*/docs/*' -name "*.png" | xargs -I{} cp {} --parents -t ./pages/docs/help/
+find ./constellation/* -path '*/docs/*' -name "*.jpg" | xargs -I{} cp {} --parents -t ./pages/docs/help
+echo "done moving."
