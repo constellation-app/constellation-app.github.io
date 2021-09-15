a6c500369a47560866b446b833f22dce7a7804fd
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-09-15 15:28:56 +0930

    Update file_copy.sh

--- .github/workflows/file_copy.sh
+++ .github/workflows/file_copy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "Moving all html png jpg to pages/docs"; find ./constellation/* -path '*/docs/*' ! -path ./constellation/docs/* -name "*html" | xargs -I{} cp {} --parents -t ./pages/docs/help/; find ./constellation/* -path '*/docs/*' ! -path ./constellation/docs/* -name "*.png" | xargs -I{} cp {} --parents -t ./pages/docs/help/; find ./constellation/* -path '*/docs/*' ! -path ./constellation/docs/* -name "*.jpg" | xargs -I{} cp {} --parents -t ./pages/docs/help;
+echo "Moving all html png jpg to pages/docs"; find ./constellation/* -path '*/docs/*' -name "*html" | xargs -I{} cp {} --parents -t ./pages/docs/help/; find ./constellation/* -path '*/docs/*' -name "*.png" | xargs -I{} cp {} --parents -t ./pages/docs/help/; find ./constellation/* -path '*/docs/*' -name "*.jpg" | xargs -I{} cp {} --parents -t ./pages/docs/help;
