e6825be82679e28ee60635fa393bbc271f2f6659
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-09-15 10:39:44 +0930

    trigger 11

--- .github/workflows/file_copy.sh
+++ .github/workflows/file_copy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "Moving all html png jpg to pages/docs"; find */src/ -path '*/docs/*' -name "*html" | xargs -I{} cp {} --parents -t ./pages/docs/constydocs/; find */src/ -path '*/docs/*' -name "*.png" | xargs -I{} cp {} --parents -t ./pages/docs/constydocs/; find */src/ -path '*/docs/*' -name "*.jpg" | xargs -I{} cp {} --parents -t ./pages/docs/constydocs/;
+echo "Moving all html png jpg to pages/docs"; find ./const/* -path '*/docs/*' -name "*html" | xargs -I{} cp {} --parents -t ./pages/docs/constydocs/; find ./const/* -path '*/docs/*' -name "*.png" | xargs -I{} cp {} --parents -t ./pages/docs/constydocs/; find ./const/* -path '*/docs/*' -name "*.jpg" | xargs -I{} cp {} --parents -t ./pages/docs/constydocs;
