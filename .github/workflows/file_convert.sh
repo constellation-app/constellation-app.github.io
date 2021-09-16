bcac7f60b0e2cbf8e8ccb53e438fe9abadd8e01f
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-09-16 09:44:51 +0930

    refactor changes

--- .github/workflows/file_convert.sh
+++ .github/workflows/file_convert.sh
@@ -3,8 +3,8 @@
 echo "converting links from .md to .html"
 find ./constellation/* -path '*docs/*' -name "*.md" | xargs -I{} sed -i -e 's/.md/.html/g' {}
 echo "converting .md to .html using template"
-find ./constellation/* -path '*/docs/*' -name "*.md" | xargs -I{} pandoc --template="./constellation/onlinePandocTemplate.txt" -f commonmark -t html {}.html
+find ./constellation/* -path '*/docs/*' -name "*.md" | while read x; do pandoc --template="./constellation/onlinePandocTemplate.txt" -f commonmark -t html $x > "$x".html; done
 echo "done converting...."
 echo "renaming all .md.html to .html"
 find ./constellation/* -name *.md.html | xargs -I{} mv {} {}.html
-echo "done renaming...."
+echo "done renaming....";
--- .github/workflows/file_copy.sh
+++ .github/workflows/file_copy.sh
@@ -3,5 +3,5 @@
 echo "Moving all html png jpg to pages/docs/help..."
 find ./constellation/* -path '*/docs/*' -name "*html" | xargs -I{} cp {} --parents -t ./pages/docs/help/
 find ./constellation/* -path '*/docs/*' -name "*.png" | xargs -I{} cp {} --parents -t ./pages/docs/help/
-find ./constellation/* -path '*/docs/*' -name "*.jpg" | xargs -I{} cp {} --parents -t ./pages/docs/help
+find ./constellation/* -path '*/docs/*' -name "*.jpg" | xargs -I{} cp {} --parents -t ./pages/docs/help/
 echo "done moving."
