4069c6f96c9314054db0807061601b07d82ee708
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-09-16 10:37:56 +0930

    refactor actions

--- .github/workflows/file_copy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Moving all html png jpg to pages/docs/help..."
-find ./constellation/* -path '*/docs/*' -name "*html" | xargs -I{} cp {} --parents -t ./pages/docs/help/
-find ./constellation/* -path '*/docs/*' -name "*.png" | xargs -I{} cp {} --parents -t ./pages/docs/help/
-find ./constellation/* -path '*/docs/*' -name "*.jpg" | xargs -I{} cp {} --parents -t ./pages/docs/help/
-echo "done moving."
