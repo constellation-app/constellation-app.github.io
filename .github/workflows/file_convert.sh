141cd5be2b842e59d1e664c4a93e6b41b035dc4e
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-09-16 09:48:10 +0930

    Update file_convert.sh

--- .github/workflows/file_convert.sh
+++ .github/workflows/file_convert.sh
@@ -6,5 +6,5 @@ echo "converting .md to .html using template"
 find ./constellation/* -path '*/docs/*' -name "*.md" | while read x; do pandoc --template="./constellation/onlinePandocTemplate.txt" -f commonmark -t html $x > "$x".html; done
 echo "done converting...."
 echo "renaming all .md.html to .html"
-find ./constellation/* -name *.md.html | xargs -I{} mv {} {}.html
+find ./constellation/* -name *.md.html | while read x; do  mv "$x" "${x%.md.html}".html; done
 echo "done renaming....";
