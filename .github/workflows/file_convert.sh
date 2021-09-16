15b2a83d176a2b468aa7bf346203ab203b388486
aldebaran30701 <58677759+aldebaran30701@users.noreply.github.com>
2021-09-16 09:32:55 +0930

    refactor

--- .github/workflows/file_convert.sh
+++ .github/workflows/file_convert.sh
@@ -3,7 +3,7 @@
 echo "converting links from .md to .html"
 find ./constellation/* -path '*docs/*' -name "*.md" | xargs -I{} sed -i -e 's/.md/.html/g' {}
 echo "converting .md to .html using template"
-find ./constellation/* -path '*/docs/*' -name "*.md" | xargs -I{} pandoc --template="./constellation/onlinePandocTemplate.txt" -f commonmark -t html {} > {}.html
+find ./constellation/* -path '*/docs/*' -name "*.md" | xargs -I{} pandoc --template="./constellation/onlinePandocTemplate.txt" -f commonmark -t html {}.html
 echo "done converting...."
 echo "renaming all .md.html to .html"
 find ./constellation/* -name *.md.html | xargs -I{} mv {} {}.html
